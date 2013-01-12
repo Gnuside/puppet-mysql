55d8db08ef82080974256e3b3098b9aeab53ea6e
Glenn Y. Rolland <glenn.rolland@gnuside.com>
2013-01-12 15:46:38 +0100

    Split in parts && add database creation.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,26 @@
 
-## INSTALL MYSQL
 
+## ENGLOBE MYSQL
 class mysql {
+    include mysql::install, mysql::service
+}
+
+class mysql::params {
   $data_path = "/var/lib/mysql"
-  package { "mysql-server": ensure => installed }
-  package { "mysql-client": ensure => installed }
+}
+
+
+## INSTALL MYSQL
+class mysql::install {
+    include mysql::params
+
+    package { "mysql-server": ensure => installed }
+    package { "mysql-client": ensure => installed }
+}
+
+## RUN MYSQL
+class mysql::service {
+    include mysql::params
 
   service { "mysql":
     enable => true,
@@ -38,6 +54,25 @@ define mysql::password (
     unless => "mysqladmin -u${username} -p'${password}' status",
     path => ["/bin", "/usr/bin"],
     command => "mysqladmin -u${username} password '${password}'",
-    require => Service['mysql']
+    require => Class['mysql::service']
   }
+}
+
+define mysql::create_database ( $username, $password, $root_password ) {
+  include mysql
+
+  $create_sql = "create database ${name};"
+  $grant_sql = "grant all on ${name}.* to ${username}@localhost identified by '$password';"
+
+   exec { "create-${name}-db":
+        unless => "/usr/bin/mysql -uroot -p${root_password} ${name}",
+        command => "/usr/bin/mysql -uroot -p${root_password} -e \"${create_sql};\"",
+	require => Class['mysql::service']
+      }
+
+      exec { "grant-${name}-db":
+        unless => "/usr/bin/mysql -u${username} -p${password} ${name}",
+        command => "/usr/bin/mysql -uroot -p${root_password} -e \"${grant_sql}\"",
+        require => [Class["mysql::service"], Exec["create-${name}-db"]]
+      }
 }
